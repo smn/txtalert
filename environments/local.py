61695d72cc2c8d9757de6d35dfa24ed9806c2bfb
shaunsephton <shaunsephton@gmail.com>
2012-01-19 08:37:42 +0200

    use local standalone sentry instance for logging

--- environments/local.py
+++ environments/local.py
@@ -9,3 +9,9 @@ DATABASES = {
         'NAME': 'txtalert_dev',
     }
 }
+
+client = Client(
+        servers=['http://localhost:4567/api/store/'], 
+        key='my69eAMYjzqtmfaRJ107MeXCYDTaxQdNZPr8YOe/zOV5pIUoZa5biA=='
+    )
+setup_logging(SentryHandler(client))
