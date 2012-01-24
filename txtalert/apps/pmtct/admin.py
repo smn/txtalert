c393405263b90a7302d1218a36195f0a2578abeb
shaunsephton <shaunsephton@gmail.com>
2012-01-24 15:23:54 +0200

    moved pmtct app to own repo/package

--- txtalert/apps/pmtct/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from txtalert.apps.pmtct.models import ExternalUID, Patient, Site
-
-admin.site.register(ExternalUID)
-admin.site.register(Patient)
-admin.site.register(Site)
